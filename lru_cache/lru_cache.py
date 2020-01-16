d4ee9fa7b6db26aadc3b0945a08c2017560c9d97
Levi-Huynh <Levi_huynh@yahoo.com>
2020-01-16 11:59:09 -0800

    lru notes

--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -8,25 +8,36 @@ from doubly_linked_list import DoublyLinkedList
 #look up for O(1) is dict
 
 #add lru to front , mru to back 
+
+#dict to help keep key value pair, another DS to keep priority order
+
+#
 class LRUCache:
     """
-    Our LRUCache class keeps track of the max number of nodes it
-    can hold, the current number of nodes it is holding, a doubly-
-    linked list that holds the key-value entries in the correct
-    order, as well as a storage dict that provides fast access
+    -Our LRUCache class keeps track of the *max number of nodes it
+    can hold*,
+    
+    -the current number of nodes it is holding, 
+    
+    -a doubly- linked list (DLL) that holds the key-value entries in the correct
+    order
+    
+    -as well as a storage dict that provides fast access
     to every node stored in the cache.
     """
-    def __init__(self, limit=10):
+    def __init__(self, limit=10): #SET LIMIT HERE IN INIT 
         self.storage = dict()
         self.limit = limit #purge from storage
         self.size = 0
-        self.order = DoublyLinkedList()
+        self.order = DoublyLinkedList() #Not passing anything here yet
     """
-    Retrieves the value associated with the given key. Also
-    needs to move the key-value pair to the end of the order
-    such that the pair is considered most-recently used.
-    Returns the value associated with the key or None if the
-    key-value pair doesn't exist in the cache.
+    -Retrieves the value associated with the given key. 
+    
+    -needs to move the key-value pair to the end of the order
+    such that the pair is considered most-recently used (MRU)
+    
+    -Returns the value associated with the key or None if the
+        key-value pair doesn't exist in the cache.
     """
     def get(self, key):
         # if no key in storage, return None
@@ -36,17 +47,22 @@ class LRUCache:
         node = self.storage[key] 
 
         # move key to tail
-        self.order.move_to_end(node)
+        self.order.move_to_end(node) 
         
-        return node.value[1]
+        return node.value[1] #??? self.storage[key].value[1] why 1? 
 
     """
-    Adds the given key-value pair to the cache. The newly-
-    added pair should be considered the most-recently used
-    entry in the cache. If the cache is already at max capacity
-    before this entry is added, then the oldest entry in the
-    cache needs to be removed to make room. Additionally, in the
-    case that the key already exists in the cache, we simply
+    Adds the given key-value pair to the cache. 
+    
+    -The *NEWLY ADDED PAIR* should be considered the most-recently used (MRU)
+    entry in the cache. 
+    
+    -CONDITIONAL If the cache is already at *max capacity*
+    BEFORE this entry is ADDED, then the OLDEST ENTRY in the
+    CACHE needs to be REMOVED to make room.
+    
+    -Additionally, CONDITIONALLY : 
+    IF the key already exists in the cache, we simply
     want to overwrite the old value associated with the key with
     the newly-specified value.
     """
@@ -57,8 +73,8 @@ class LRUCache:
             # update the value in storage
             node.value = (key, value)
             # move the existing node to the tail
-            self.order.move_to_end(node)
-            return
+            self.order.move_to_end(node) # self.order = DoublyLinkedList()
+            return #what does that actually RETURN? 
 
         # the key does not exist
         # create a node with the key
@@ -70,7 +86,7 @@ class LRUCache:
         # check if limit exceeded 
         if self.size > self.limit:
             # remove the least recently used key: value pair
-            del self.storage[self.order.head.value[0]]
+            del self.storage[self.order.head.value[0]] #why value here is [0]??
             # remove current head from order
             self.order.remove_from_head()
             self.size -= 1
